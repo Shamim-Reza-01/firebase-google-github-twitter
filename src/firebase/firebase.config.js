467cd2e2e8ddbf107a0842796c6d78610fe45ec0
shamim <rdshamim449@gmail.com>
2023-09-29 05:27:41 +0600

    fast commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAbAYAZegQBtf_bG-JzSQ83htBxyYERD9o",
+  authDomain: "fir-githud-twitter.firebaseapp.com",
+  projectId: "fir-githud-twitter",
+  storageBucket: "fir-githud-twitter.appspot.com",
+  messagingSenderId: "407043111270",
+  appId: "1:407043111270:web:8ddf8a67881662584f30a7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
